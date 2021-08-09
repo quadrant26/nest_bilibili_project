eb27959fc0298fb129de096ab8119b8ea179b50a
quadrant26 <quadrant26@163.com>
2021-08-09 19:14:19 +0800

    feat: 用户注册

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { User } from 'src/interface/user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('regist')
+  @ApiOperation({ summary: '用户注册' })
+  async registerUser(@Body() userDto: User) {
+    return await this.userService.register(userDto);
+  }
+}
