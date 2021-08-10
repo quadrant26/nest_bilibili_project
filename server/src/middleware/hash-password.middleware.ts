4a2640149f0e04163681f56c922b202daf07397f
quadrant26 <quadrant26@163.com>
2021-08-10 17:11:38 +0800

    feat: 用户密码加密中间件

--- server/src/middleware/hash-password.middleware.ts
+++ server/src/middleware/hash-password.middleware.ts
@@ -1,9 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Response, Request } from 'express';
+import { addSalt, encript } from '../utils/Encription';
 
 @Injectable()
 export class HashPasswordMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    let userPassword = req.body['password'];
+    // 对用户密码进行加密
+    if (userPassword) {
+      userPassword = encript(userPassword, addSalt());
+      req.body['password'] = userPassword;
+    }
+    console.log(req.body);
     next();
   }
 }
