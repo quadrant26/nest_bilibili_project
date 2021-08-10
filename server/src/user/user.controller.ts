d1027c494d14add03945b9905eaf940d7d358f64
quadrant26 <quadrant26@163.com>
2021-08-10 19:00:49 +0800

    feat: 使用 auth 守卫

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Body, Post } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import {
+  Controller,
+  Body,
+  Post,
+  UseGuards,
+  Get,
+  SetMetadata,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/interface/user.interface';
 import { UserService } from './user.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { Role } from 'src/role/role.decorator';
 
 @Controller('user')
+@ApiTags('用户模块')
+@UseGuards(AuthGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
@@ -11,5 +22,12 @@ export class UserController {
   @ApiOperation({ summary: '用户注册' })
   async registerUser(@Body() userDto: User) {
     return await this.userService.register(userDto);
+  }
+
+  @Get('hello')
+  // @SetMetadata('roles', ['admin']) // 设置元数据， 设置接口的权限
+  @Role('admin') // 自定义装饰器
+  hello() {
+    return 'hello world';
   }
 }
