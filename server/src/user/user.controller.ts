1f43a8b37d601c1b1f6ab9612c04e93ef9d6ab65
quadrant26 <quadrant26@163.com>
2021-08-11 15:05:41 +0800

    chroe: 删除自定义守卫， AuthGuards('jwt)

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -9,18 +9,18 @@ import {
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/interface/user.interface';
 import { UserService } from './user.service';
-import { AuthGuard } from '../guards/auth.guard';
 import { Role } from 'src/role/role.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 @ApiTags('用户模块')
-@UseGuards(AuthGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('hello')
   // @SetMetadata('roles', ['admin']) // 设置元数据， 设置接口的权限
   @Role('admin') // 自定义装饰器
+  // @UseGuards(AuthGuard('jwt'))
   hello() {
     return 'hello world';
   }
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,17 +2,9 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { HashPasswordMiddleware } from '../middleware/hash-password.middleware';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from 'src/guards/auth.guard';
 
 @Module({
-  providers: [
-    UserService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
+  providers: [UserService],
   controllers: [UserController],
 })
 export class UserModule implements NestModule {
