aa6b94b23337284eea8816d846edb42597953c2c
quadrant26 <quadrant26@163.com>
2021-08-11 15:07:11 +0800

    chroe: jwt 设置

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { JWT_CONSTANT } from './jwt.constants';
+import { User } from 'src/interface/user.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: JWT_CONSTANT.secret,
+    });
+  }
+
+  async validate(payload: User) {
+    return { userId: payload._id };
+  }
+}
