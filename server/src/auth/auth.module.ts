8636cb7a8b23fdd2dfeff0321fbc8e44a185241b
quadrant26 <quadrant26@163.com>
2021-08-11 17:18:39 +0800

    feat: 注册和改密都需要 HashPasswordMiddleware

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from '../user/user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JWT_CONSTANT } from './jwt.constants';
 import { JwtStrategy } from './jwt.strategy';
+import { HashPasswordMiddleware } from '../middleware/hash-password.middleware';
 
 @Module({
   imports: [
@@ -15,4 +16,12 @@ import { JwtStrategy } from './jwt.strategy';
   providers: [AuthService, UserService, JwtStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(HashPasswordMiddleware)
+      .forRoutes('auth/regist')
+      .apply(HashPasswordMiddleware)
+      .forRoutes('auth/alter');
+  }
+}
