fc89b5b994a6926aa852a6c12b8b49c7c56fdba6
quadrant26 <quadrant26@163.com>
2021-08-11 14:27:34 +0800

    chroe: UserScahema prop 默认值修改

--- server/src/interface/user.interface.ts
+++ server/src/interface/user.interface.ts
@@ -5,11 +5,11 @@ import { ApiProperty } from '@nestjs/swagger';
 @Schema()
 export class User extends Document {
   @Prop()
-  @ApiProperty({ description: '手机号', example: '18688924563' })
+  @ApiProperty({ description: '手机号', example: '10000000001' })
   readonly phone: string;
 
   @Prop()
-  @ApiProperty({ description: '密码', example: '132456' })
+  @ApiProperty({ description: '密码', example: '123456' })
   readonly password: string;
 
   @Prop()
