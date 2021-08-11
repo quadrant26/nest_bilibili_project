4a8d4b1b92dbd850bce7ad27487427c46df0bff9
quadrant26 <quadrant26@163.com>
2021-08-11 15:32:06 +0800

    feat: 用户模块全局守卫

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -6,13 +6,15 @@ import {
   Get,
   SetMetadata,
 } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/interface/user.interface';
 import { UserService } from './user.service';
 import { Role } from 'src/role/role.decorator';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth('jwt')
 @ApiTags('用户模块')
 export class UserController {
   constructor(private userService: UserService) {}
@@ -20,7 +22,6 @@ export class UserController {
   @Get('hello')
   // @SetMetadata('roles', ['admin']) // 设置元数据， 设置接口的权限
   @Role('admin') // 自定义装饰器
-  // @UseGuards(AuthGuard('jwt'))
   hello() {
     return 'hello world';
   }
