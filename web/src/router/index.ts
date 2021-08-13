8a2e5b6700ce4b710b4e46066183e386c2deaa9f
quadrant26 <quadrant26@163.com>
2021-08-13 13:30:49 +0800

    upd: init router-view

---
+++ web/src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Login from "../views/Login.vue";
+
+const routes = [{ path: "/", component: Login }];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+});
+
+export default router;
---
+++ web/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    123234
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
