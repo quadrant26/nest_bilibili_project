3752618a332a777c31214c1461ff22dbf18265f4
quadrant26 <quadrant26@163.com>
2021-08-13 13:39:52 +0800

    chroe: yarn add element-plus and how use

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(router).use(store).mount("#app");
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+
+createApp(App).use(router).use(store).use(ElementPlus).mount("#app");
