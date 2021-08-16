426a30109db5b86303717060c32259166df6ce44
quadrant26 <quadrant26@163.com>
2021-08-16 16:47:58 +0800

    chroe: 创建 nest project 模块

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { DbModule } from './db/db.module';
 import { UserModule } from './user/user.module';
 import { Log4jsModule } from '@nestx-log4js/core';
 import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [DbModule, UserModule, Log4jsModule.forRoot(), AuthModule],
+  imports: [DbModule, UserModule, Log4jsModule.forRoot(), AuthModule, ProjectModule],
   controllers: [AppController],
   providers: [AppService],
 })
