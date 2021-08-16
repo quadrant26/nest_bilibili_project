02c69864fa2490e15dbda0a82d0c5b515925bef5
quadrant26 <quadrant26@163.com>
2021-08-16 17:27:41 +0800

    feat: 项目的更删改查接口

--- server/src/project/project.controller.ts
+++ server/src/project/project.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Project } from 'src/interface/project.interface';
 import { ProjectService } from './project.service';
@@ -12,5 +12,26 @@ export class ProjectController {
   @ApiOperation({ summary: '创建新项目' })
   async addProject(@Body() project: Project) {
     return this.projectService.createProject(project);
+  }
+
+  @Post('delete/:id')
+  @ApiOperation({ summary: '删除项目' })
+  async removeProject(@Param('id') projectId: string) {
+    return this.projectService.deleteProject(projectId);
+  }
+
+  @Post('update/:id')
+  @ApiOperation({ summary: '更新项目 ' })
+  async updateProject(
+    @Param('id') projectId: string,
+    @Body() project: Project,
+  ) {
+    return this.projectService.alterProject(projectId, project);
+  }
+
+  @Post('find/:id')
+  @ApiOperation({ summary: '查询项目' })
+  async findOneById(@Param('id') projectId: string) {
+    return this.projectService.findProjectById(projectId);
   }
 }
--- server/src/project/project.service.ts
+++ server/src/project/project.service.ts
@@ -32,4 +32,59 @@ export class ProjectService {
       return this.response;
     }
   }
+
+  async deleteProject(projectId: string) {
+    try {
+      await this.projectModel.findByIdAndDelete(projectId);
+      this.response = {
+        code: 0,
+        msg: '项目删除成功',
+      };
+    } catch (error) {
+      logger.warn('项目删除失败');
+      this.response = {
+        code: 5,
+        msg: '删除失败',
+      };
+    } finally {
+      return this.response;
+    }
+  }
+
+  async alterProject(projectId: string, project: Project) {
+    try {
+      await this.projectModel.findByIdAndUpdate(projectId, project);
+      this.response = {
+        code: 0,
+        msg: '更新成功',
+      };
+    } catch (error) {
+      logger.warn('项目更新失败');
+      this.response = {
+        code: 5,
+        msg: '更新失败',
+      };
+    } finally {
+      return this.response;
+    }
+  }
+
+  async findProjectById(projectId: string) {
+    try {
+      const data = await this.projectModel.findById(projectId);
+      this.response = {
+        code: 0,
+        msg: '查询成功',
+        data,
+      };
+    } catch (error) {
+      logger.warn('查询失败');
+      this.response = {
+        code: 5,
+        msg: '查询失败',
+      };
+    } finally {
+      return this.response;
+    }
+  }
 }
